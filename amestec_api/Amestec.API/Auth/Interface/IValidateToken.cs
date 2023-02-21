e616503ba324664166583419529d23400931f2df
Stefan Dornean <stefan.dornean@int.expertware.net>
2023-02-22 00:03:34 +0200

    publish

--- amestec_api/Amestec.API/Auth/Interface/IValidateToken.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-
-namespace Amestec.API.Auth.Interface
-{
-    public interface IValidateToken
-    {
-        Task ValidateAsync(TokenValidatedContext context);
-    }
-}
